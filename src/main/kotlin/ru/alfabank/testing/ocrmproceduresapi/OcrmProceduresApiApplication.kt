ade528236f41c9cb2b55eb71f507d5dd710efd63
Artem Ptushkin <aptushkin@alfabank.ru>
2019-07-23 14:34:19 +0300

    initial commit by Spring initializer

---
+++ src/main/kotlin/ru/alfabank/testing/ocrmproceduresapi/OcrmProceduresApiApplication.kt
@@ -0,0 +1,11 @@
+package ru.alfabank.testing.ocrmproceduresapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OcrmProceduresApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<OcrmProceduresApiApplication>(*args)
+}
