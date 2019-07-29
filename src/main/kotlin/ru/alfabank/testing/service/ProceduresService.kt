6271eae5005af7386e590c26197a5fd44af0447c
Artem Ptushkin <aptushkin@alfabank.ru>
2019-07-29 15:46:03 +0300

    Added procedureRequest validator to the service

--- src/main/kotlin/ru/alfabank/testing/service/ProceduresService.kt
+++ src/main/kotlin/ru/alfabank/testing/service/ProceduresService.kt
@@ -2,16 +2,19 @@ package ru.alfabank.testing.service
 
 import org.springframework.jdbc.core.simple.SimpleJdbcCall
 import org.springframework.stereotype.Service
+import org.springframework.validation.annotation.Validated
 import ru.alfabank.testing.configuration.ProcedureProperties
 import ru.alfabank.testing.configuration.ProcedureProperty
 import ru.alfabank.testing.domain.ProcedureRequest
+import javax.validation.Valid
 
 @Service
+@Validated
 class ProceduresService(
         private val procedureProperties: ProcedureProperties, private val procedureNameToJdbcCall: Map<String, SimpleJdbcCall>
 ) {
 
-    fun execute(procedureRequest: ProcedureRequest) {
+    fun execute(@Valid procedureRequest: ProcedureRequest) {
         val procedure: ProcedureProperty = procedureProperties.properties.getValue(procedureRequest.name)
 
         val jdbcCallParameters = procedure.parameters
