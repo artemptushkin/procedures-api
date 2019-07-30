70e4a0c91c4fbd8e8725314f094bad36d77ce527
Artem Ptushkin <aptushkin@alfabank.ru>
2019-07-30 13:28:58 +0300

    added docker plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,12 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import com.bmuschko.gradle.docker.tasks.image.*
 
 buildscript {
 	repositories {
 		maven("http://binary/artifactory/public")
+	}
+	dependencies {
+		classpath("com.bmuschko:gradle-docker-plugin:3.2.5")
 	}
 }
 
@@ -12,6 +16,8 @@ plugins {
 	kotlin("jvm") version "1.2.71"
 	kotlin("plugin.spring") version "1.2.71"
 }
+
+apply(plugin = "com.bmuschko.docker-remote-api")
 
 group = "ru.alfabank.testing"
 version = "0.0.1-SNAPSHOT"
@@ -29,6 +35,19 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.register<Dockerfile>("createDockerfile") {
+	from("openjdk:jre-alpine")
+	copyFile("build/libs/${project.name}-${project.version}", "${project.name}-${project.version}.jar")
+	exposePort(9090)
+	defaultCommand("-jar ${project.name}-${project.version}.jar")
+}
+
+tasks.register<DockerBuildImage>("buildImage") {
+	dependsOn("createDockerfile")
+	inputDir = project.file("build/docker")
+	tags.add("procedure-api")
 }
 
 tasks.withType<KotlinCompile> {
